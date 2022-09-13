12f5358982e001cc4af52afe2b5d3463acb8a1a5
Alexandra <sobakasasha114@gmail.com>
2022-09-13 17:49:45 +0300

    init commit

---
+++ src/test/java/com/volushkova/apifirst/apifirstclient/ApiFirstClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volushkova.apifirst.apifirstclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFirstClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
