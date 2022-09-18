ff7305f038ce41c8887a31c996453e82aaae3e63
Alexandra <sobakasasha114@gmail.com>
2022-09-18 19:58:30 +0300

    fix client generation. add sample implementation

--- src/main/java/com/volushkova/apifirst/apifirstclient/controller/TestController.java
+++ src/main/java/com/volushkova/apifirst/apifirstclient/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.volushkova.apifirst.apifirstclient.controller;
 
+
 import com.volushkova.apifirst.generated.petclient.PetsApiClient;
+import com.volushkova.apifirst.generated.petclient.model.Pet;
 import com.volushkova.apifirst.generated.server.TestsApi;
 import com.volushkova.apifirst.generated.server.model.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 public class TestController implements TestsApi {
@@ -16,7 +20,19 @@ public class TestController implements TestsApi {
 
     @Override
     public ResponseEntity<Test> showTestById(String testId) {
-
-        return TestsApi.super.showTestById(testId);
+        petsApiClient.createPets();
+        ResponseEntity<List<Pet>> listResponseEntity = petsApiClient.listPets(10);
+        List<Pet> petList = listResponseEntity.getBody();
+        StringBuilder result = new StringBuilder();
+        if (petList != null) {
+            for (Pet pet : petList) {
+                result.append(pet.getName());
+            }
+        }
+        Test test = new Test();
+        test.setId(1L);
+        test.setTag(result.toString());
+        test.setName(result.toString());
+        return ResponseEntity.ok(test);
     }
 }
